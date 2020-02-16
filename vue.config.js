47dc4f50c0efd25784e5b326ff27046eca59a22f
dssho <1824675146@qq.com>
2020-02-16 17:47:37 +0800

    zuizhong

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports={
     proxy:{
       // 反向代理
       '/v3':{
-        target:"http://localhost:3000",
+        target:"http://localhost:5050",
         changeOrigin:true
       },
       // 因为之后都是以v2为分界点
@@ -14,11 +14,31 @@ module.exports={
         changeOrigin:true
       }
     },
-    public:'192.168.0.107:8080',
+    public:'192.168.0.107:8080/dy',
     hot:true,
     disableHostCheck:true,
-   
-     
-    
+  },
+  //webpack配置
+	configureWebpack: {
+    //关闭 webpack 的性能提示
+    performance: {
+      hints:false
+    },
+
+    //或者
+
+    //警告 webpack 的性能提示
+    performance: {
+      hints:'warning',
+      //入口起点的最大体积
+      maxEntrypointSize: 50000000,
+      //生成文件的最大体积
+      maxAssetSize: 30000000,
+      //只给出 js 文件的性能提示
+      assetFilter: function(assetFilename) {
+        return assetFilename.endsWith('.js');
+      }
+    }
   }
+
 }
